b2409fe4263064f23ad5d5611949458c2acdc7e8
navsingh3211 <navsingh3211@gmail.com>
2023-04-15 09:22:15 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const app = require('./app');
+
+const dotenv=require('./dotenv');
+
+
+//Config
+
+dotenv.config({path:"backend/config/config.env"});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is working on port ${process.env.PORT}`);
+});
